abc3a1e1d6d89becab5ca8980d61330615a24dbd
Xiao-huochai <chai437437@gmail.com>
2026-03-01 11:23:53 +0800

    新闻列表样式结构完成

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import { Image } from "antd-mobile";
+import "./NewsItem.less";
 export default function NewsItem() {
-  return <div className="news-item-box">每条新闻</div>;
+  return (
+    <div className="news-item-box">
+      <Link to={{ pathname: "/detail/xxx" }}>
+        <div className="content">
+          <h4 className="title">
+            oioi你所热爱的就是你oioi你所热爱的就是你的生活oioioioioioioio的生活oioioioioioioio
+          </h4>
+          <div className="author">hajimi</div>
+        </div>
+        <Image
+          src="https://picx.zhimg.com/v2-e59aec8b594d46307317406b09ad4243.jpg?source=8673f162"
+          lazy
+        ></Image>
+      </Link>
+    </div>
+  );
 }
