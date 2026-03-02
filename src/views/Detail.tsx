997bb5132af4cd7d93d1b4167947154492d2ff2f
Xiao-huochai <chai437437@gmail.com>
2026-03-02 09:03:00 +0800

    详情页底部导航朗样式书写

--- src/views/Detail.tsx
+++ src/views/Detail.tsx
@@ -1,4 +1,41 @@
+import "./Detai.less";
+import {
+  LeftOutline,
+  MessageOutline,
+  StarOutline,
+  LikeOutline,
+  MoreOutline,
+} from "antd-mobile-icons";
+import { Badge } from "antd-mobile";
+import { useNavigate } from "react-router-dom";
+
 const Detail = function Detail() {
-  return <div className="detail-box">详情页</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="detail-box">
+      {/* 内容 */}
+      <div className="content"></div>
+      {/* 底部图标 */}
+      <div className="tab-bar">
+        <div className="back" onClick={() => navigate(-1)}>
+          <LeftOutline></LeftOutline>
+        </div>
+        <div className="icons">
+          <Badge content="128">
+            <MessageOutline></MessageOutline>
+          </Badge>
+          <Badge content="29">
+            <LikeOutline></LikeOutline>
+          </Badge>
+          <span>
+            <StarOutline></StarOutline>
+          </span>
+          <span>
+            <MoreOutline></MoreOutline>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Detail;
